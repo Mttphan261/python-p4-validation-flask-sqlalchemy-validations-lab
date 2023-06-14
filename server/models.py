be8e6583d7eddfc0e3b03b6684cfec88c18400f5
Matthew Phan <mttphan261@gmail.com>
2023-06-14 06:06:07 -0500

    updated returns

--- server/models.py
+++ server/models.py
@@ -42,22 +42,26 @@ class Post(db.Model):
     def validate_content(self, key, content_str):
         if len(content_str) < 250:
             raise ValueError("Content must be at least 250 characters")
+        return content_str
         
     @validates('summary')
     def validate_summary(self, key, summary_str):
         if len(summary_str) >= 250:
             raise ValueError("Content cannot be over 250 characters")
+        return summary_str
     
     @validates('category')
     def validates_category(self, key, cat_str):
         if cat_str != 'Fiction' and cat_str != 'Non-Fiction':
             raise ValueError("Category must be either Fiction or Non-fiction")
+        return cat_str
     
     @validates('title')
     def validates_title(self, key, title_str):
         clickbait = ["Won't Believe","Secret","Top","Guess"]
         if not any(word in title_str for word in clickbait):
             raise ValueError('Title is not clickbaity enough')
+        return title_str
 
 
     def __repr__(self):
